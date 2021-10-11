4d787a2523f2873e6dedab7bd6708525b1a48825
sXakil <pau.shakil@gmail.com>
2021-10-12 04:09:47 +0600

    roadmap, faq and typed added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import Typed from "typed.js";
 import logo from "../images/space_babez_logo.png";
 
 export default function Header({ onBuyButtonClicked }) {
   const [ft, setFt] = useState(0);
   const [timer, setTimer] = useState("00:00:00");
+  const text = useRef(null);
+  const typed = useRef(null);
   useEffect(() => {
     setFt(Math.floor(Math.random() * 14 + 1));
-    const countTo = new Date("Oct 13, 2021 11:59:59").getTime();
+    const countTo = new Date("Oct 17, 2021 11:59:59").getTime();
     const interval = setInterval(() => {
       const distance = countTo - new Date().getTime();
       setTimer(
@@ -20,8 +23,23 @@ export default function Header({ onBuyButtonClicked }) {
       );
       if (distance < 0) clearInterval(interval);
     }, 1000);
-    return () => clearInterval(interval);
+    return () => {
+      typed.current?.destroy();
+      clearInterval(interval);
+    };
   }, []);
+  function resetTyped() {
+    typed.current = new Typed(text.current, {
+      strings: [
+        "9,999 crypt",
+        '<span class="pink">9,999</span> crypto collectibles on the Cardano blockchain',
+      ],
+      typeSpeed: 40,
+      backSpeed: 30,
+      backDelay: 0,
+      showCursor: false,
+    });
+  }
   return (
     <div className="main-header">
       <div className="header-container">
@@ -37,7 +55,12 @@ export default function Header({ onBuyButtonClicked }) {
           <h1 className="slogan">
             If you're gonna be hot, you might as well be hot in space...
           </h1>
-          <img src={`/ft/${ft || 0}.png`} className="babez" alt="BABEZ 5" />
+          <img
+            src={`/ft/${ft || 0}.png`}
+            className="babez"
+            alt="BABEZ 5"
+            onLoad={resetTyped}
+          />
           <button
             className="buyNow backdrop"
             style={{ display: "none" }}
@@ -47,10 +70,7 @@ export default function Header({ onBuyButtonClicked }) {
           </button>
           <span className="label">Sale starts in</span>
           <span className="time">{timer}</span>
-          <h1>
-            <span className="pink">9,999</span> crypto collectibles on the
-            Cardano blockchain
-          </h1>
+          <h1 aria-hidden="true" ref={text} />
         </div>
       </div>
     </div>
