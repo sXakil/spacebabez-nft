4baaed2ca5316231d03237faabb4cef1da1764e2
sXakil <pau.shakil@gmail.com>
2021-10-29 04:52:41 +0600

    scripts updated

---
+++ gen_cat.py
@@ -0,0 +1,14 @@
+import json
+
+with open('generate_cat.json') as f:
+  data = json.load(f)
+
+for d in data:
+    idx = 1
+    for k in data[d]:
+        k['value'] = idx
+        print(k)
+        idx += 1
+
+with open('generate_cat.json', 'w') as file:
+    json.dump(data, file)
